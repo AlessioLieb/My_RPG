61191a68ef71f66e6ac9a402f57f066287f347e4
TheotimeS <theotime.schmeltz@epitech.eu>
2022-04-05 13:21:22 +0200

    valgrind fix

--- Procedural generation/main.c
+++ Procedural generation/main.c
@@ -11,17 +11,20 @@ int main(int ac, char **av)
 {
     srand(time(NULL));
     int x = 10;
-    char **map = malloc(sizeof(char *) * x);
+    char **map = malloc(sizeof(char *) * x + 1);
     rooms room;
     create_rooms(&room);
-    for (int i = 0; i != x; ++i)
-        map[i] = malloc(x);
+    for (int i = 0; i != x; ++i) {
+        map[i] = malloc(x + 1);
+        map[i][x] = '\0';
+    }
+    map[x] = NULL;
     for (int i = 0; i != x; ++i)
         for (int j = 0; j != x; ++j)
             map[i][j] = ' ';
     for (int j = 0; j != 5; ++j)
         print_map(map, x, &room, j);
-    for (int i = 0; i != x; ++i)
+    for (int i = 0; map[i] != NULL; ++i)
         free(map[i]);
     free(map);
     free(room.lvl_rooms);
