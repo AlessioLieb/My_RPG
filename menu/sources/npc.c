383a5ef55fc2220338086eb0a72b2f108fdc3d0a
Oscar FRANK <oscar.frank@epitech.eu>
2022-04-28 13:45:10 +0200

    (|) change npc in first room

--- menu/sources/npc.c
+++ menu/sources/npc.c
@@ -7,18 +7,16 @@
 
 #include "../includes/motor.h"
 
-void init_npc(window *wndw, options *sprt)
-{
-    sfVector2f prems = {0, 0};
+void init_npc(window *wndw, options *sprt) {
+    sfVector2f prems = {275, 65};
     sfIntRect first = {0, 0, 1920, 1080};
     sprt->npc = sfSprite_create();
     sfSprite_setTexture(sprt->npc, sprt->text_npc, sfTrue);
     sfSprite_setPosition(sprt->npc, prems);
-    printf("coucocucoudifhvufhvfiuevhre");
+    sfSprite_setScale(sprt->npc, (sfVector2f) {2, 2});
 }
 
-void draw_npc(window *wndw, options *sprt, room *rm)
-{
+void draw_npc(window *wndw, options *sprt, room *rm) {
     if (rm->actual_room[0] == 4 && rm->actual_room[1] == 4)
         sfRenderWindow_drawSprite(wndw->window, sprt->npc, NULL);
 }
