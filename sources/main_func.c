f583ccfd4de08e8480cce4715d67d3d5b4b5814b
Liebenguth Alessio <alessio.liebenguth@epitech.eu>
2022-05-03 11:18:45 +0200

    draw description HUD after npc in shop

--- sources/main_func.c
+++ sources/main_func.c
@@ -51,8 +51,8 @@ int my_game(window *wndw, sfEvent event, reduce *red, options *sprt)
     nb_piece_hud(red->py->invent.money, wndw->window);
     nb_bomb_hud(red->py->invent.bomb, wndw->window);
     nb_key_hud(red->py->invent.key, wndw->window);
-    disp_stat_hud(red, wndw->window);
     draw_npc(wndw, sprt, red);
+    disp_stat_hud(red, wndw->window);
     nb_time_hud(sfClock_getElapsedTime(red->py->time).microseconds / 1000000,
     wndw->window);
     if (red->py->new_item) {
