0da828bfefdafa055e3e018273ff8155d42b56ee
Liebenguth Alessio <alessio.liebenguth@epitech.eu>
2022-05-06 21:10:05 +0200

    function that calls the disp functions for the two volume bars

--- sources/draw_sprites4.c
+++ sources/draw_sprites4.c
@@ -5,11 +5,20 @@
 ** draw_sprites4.c
 */
 
-#include "../includes/menu.h"
+#include "../includes/music.h"
 
-void dopt_sprt(window *wndw, options *sprt)
+void is_pressed(window *wndw, options *opt, reduce *red, slide_bar *s_bar)
 {
+    sfVector2i pos = sfMouse_getPosition(NULL);
+    for (int i = 0; i < opt->len_button; ++i)
+        touch_button(wndw, opt, i, pos);
+}
+
+void dopt_sprt(window *wndw, options *sprt, reduce *red, slide_bar *s_bar)
+{
+    //sfVector2f m_pos = sfMouse_getPosition(NULL);
     sfRenderWindow_drawSprite(wndw->window, sprt->opt_sprt, NULL);
+    sound_bar_handling(red, s_bar, wndw->window);
 }
 
 void dwin_sprt(window *wndw, options *sprt)
